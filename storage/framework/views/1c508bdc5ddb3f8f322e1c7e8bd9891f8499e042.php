53c85e82abd972e8e05b4ecc6e9854892702d6c6
Lilla03 <nguyenthuphuong3958@gmail.com>
2025-05-16 16:45:11 +0700

    update new login UI

--- storage/framework/views/1c508bdc5ddb3f8f322e1c7e8bd9891f8499e042.php
+++ storage/framework/views/1c508bdc5ddb3f8f322e1c7e8bd9891f8499e042.php
@@ -1,8 +1,8 @@
-@extends('layouts.master-without-nav')
-@section('title')
-    @lang('translation.signin')
-@endsection
-@section('content')
+
+<?php $__env->startSection('title'); ?>
+    <?php echo app('translator')->get('translation.signin'); ?>
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
     <div class="container-fluit auth-bg">
         <div class=" pt-5 ">
             <!-- auth page bg -->
@@ -18,7 +18,7 @@
                                         <div
                                             class="col-lg-12 h-100 d-flex flex-column  justify-content-between text-center">
                                             <div class="d-flex justify-content-center mt-2">
-                                                <img class="w-fit" src="{{ URL::asset('assets/images/logo-dark.png') }}"
+                                                <img class="w-fit" src="<?php echo e(URL::asset('assets/images/logo-dark.png')); ?>"
                                                     class="rounded me-2" alt="..." height="80">
                                             </div>
                                             <div class="mt-4">
@@ -40,26 +40,47 @@
                                         <div class="py-2 mt-4 col">
                                             <p class="text-left text-lg text-uppercase text-blue fw-bold ">Đăng nhập</p>
 
-                                            <form action="{{ route('login') }}" method="POST">
-                                                @csrf
+                                            <form action="<?php echo e(route('login')); ?>" method="POST">
+                                                <?php echo csrf_field(); ?>
                                                 <div class="mb-3 position-relative">
                                                     <input type="text"
-                                                        class="form-control-x @error('email') is-invalid @enderror"
-                                                        value="{{ old('email', 'admin@themesbrand.com') }}"
+                                                        class="form-control-x <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                                        value="<?php echo e(old('email', 'admin@themesbrand.com')); ?>"
                                                         id="username" name="email" placeholder="Tên đăng nhập">
                                                      
                                                     <i class="ri-user-fill position-absolute top-50 start-0 translate-middle-y ps-3 icon-scale "></i>
-                                                    @error('email')
+                                                    <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
                                                         <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
+                                                            <strong><?php echo e($message); ?></strong>
                                                         </span>
-                                                    @enderror
+                                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                                 </div>
 
                                                 <div class="mb-3">
                                                     <div class="position-relative auth-pass-inputgroup mb-3">
                                                             <input type="password"
-                                                                class="form-control-x ps-5 pe-5 @error('password') is-invalid @enderror"
+                                                                class="form-control-x ps-5 pe-5 <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
                                                                 name="password" placeholder="Mật khẩu" id="password-input"
                                                                 value="123456">
                                                        
@@ -69,11 +90,18 @@
                                                             class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted"
                                                             type="button" id="password-addon"><i
                                                                 class="ri-eye-fill align-middle icon-scale "></i></button>
-                                                        @error('password')
+                                                        <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
                                                             <span class="invalid-feedback" role="alert">
-                                                                <strong>{{ $message }}</strong>
+                                                                <strong><?php echo e($message); ?></strong>
                                                             </span>
-                                                        @enderror
+                                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                                     </div>
                                                 </div>
 
@@ -109,13 +137,14 @@
     </div>
 
 
-@endsection
-@section('script')
-    <link rel="stylesheet" href="{{ url('assets/css/login.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/css/custom.css') }}">
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+    <link rel="stylesheet" href="<?php echo e(url('assets/css/login.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(url('assets/css/custom.css')); ?>">
 
-    <script src="{{ URL::asset('assets/libs/particles.js/particles.js.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/pages/particles.app.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/pages/password-addon.init.js') }}"></script>
+    <script src="<?php echo e(URL::asset('assets/libs/particles.js/particles.js.min.js')); ?>"></script>
+    <script src="<?php echo e(URL::asset('assets/js/pages/particles.app.js')); ?>"></script>
+    <script src="<?php echo e(URL::asset('assets/js/pages/password-addon.init.js')); ?>"></script>
 
-@endsection
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master-without-nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\theme_defaut\resources\views/auth/login.blade.php ENDPATH**/ ?>
